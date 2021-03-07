8f0841374548818bdea8437640da972776368ff7
cybarreto <cybarreto@indracompanmy.com>
2021-03-07 16:37:11 -0500

    sidebar cloase all fun added

--- src/app/_modules/_components/sidebar/sidebar.component.ts
+++ src/app/_modules/_components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
   public toggle: boolean = true;
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -17,5 +20,6 @@ export class SidebarComponent implements OnInit {
 
   public handleOut() {
     this.toggle = true;
+    this.accordion.closeAll();
   }
 }
