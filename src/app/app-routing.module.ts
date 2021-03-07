5b1e632260c570326372414a64e3ac26d59d3677
cybarreto <cybarreto@indracompanmy.com>
2021-03-07 17:03:02 -0500

    404 page implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './_modules/home/_page/home/home.component';
 import { LoginComponent } from './_modules/login/login.component';
+import { NotFoundComponent } from './_modules/not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,14 @@ const routes: Routes = [
     component: HomeComponent,
     loadChildren: () =>
       import('./_modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
   },
 ];
 
