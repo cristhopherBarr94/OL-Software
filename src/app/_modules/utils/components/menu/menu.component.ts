6ccafe15505aa057742dc53603b412cfedaea87a
cybarreto <cybarreto@indracompanmy.com>
2021-03-07 16:15:45 -0500

    logout button added

--- src/app/_modules/utils/components/menu/menu.component.ts
+++ src/app/_modules/utils/components/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UiService } from 'src/app/_services/ui.service';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   public userName: string = 'Andrés Felipe Garcia Castro';
-  constructor() {}
+  constructor(private router: Router, private uiService: UiService) {}
 
   ngOnInit(): void {}
+  public logout() {
+    this.uiService.openDialog(LoadingComponent, 'loading-modal', true);
+    setTimeout(() => {
+      this.uiService.closeDialog();
+      this.router.navigate(['login'], { queryParamsHandling: 'preserve' });
+    }, 2000);
+  }
 }
