9847a23db79498e5d50719861ea848b384f251d9
cybarreto <cybarreto@indracompanmy.com>
2021-03-07 16:25:08 -0500

    footer added

--- src/app/_modules/utils/utils.module.ts
+++ src/app/_modules/utils/utils.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './components/loading/loading.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
-  declarations: [LoadingComponent, MenuComponent],
+  declarations: [LoadingComponent, MenuComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [LoadingComponent, MenuComponent],
+  exports: [LoadingComponent, MenuComponent, FooterComponent],
 })
 export class UtilsModule {}
